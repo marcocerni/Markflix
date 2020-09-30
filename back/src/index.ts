7fdad96baf999088c9c95056a30f3e0168c6e43c
ivancerni <ivancerni@gmail.com>
2020-09-30 09:44:08 +0200

    Fixes improvements and new features

--- back/src/index.ts
+++ back/src/index.ts
@@ -18,8 +18,8 @@ import config from "./config/config";
         // Call midlewares
         app.use(cors());
         app.use(helmet());
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json({limit: '50mb'}));
+        app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
         // app.use(multipart());
 
 
